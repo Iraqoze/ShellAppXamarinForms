306af0986c8d5c4f9c905dde95bd023d3de88c68
Lionel Irakoze <69111482+Iraqoze@users.noreply.github.com>
2020-11-06 21:46:24 +0300

    New Changes

---
+++ ShellApp/ShellApp/ShellApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ShellApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShellApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ShellViewPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
